97ec227322aaf6c49c2c62c28e5886086c00fddf
Rafael Moraes <rafael1327ff@gmail.com>
2026-04-14 11:50:41 -0300

    feat(inserindo novo projeto)

--- Front-End/src/components/Projects/Projects.jsx
+++ Front-End/src/components/Projects/Projects.jsx
@@ -23,7 +23,129 @@ function Projects({ modoEscuro }) {
       <article
         className={modoEscuro ? "contentProjetos" : "contentProjetosWhite"}
       >
-        {" "}
+        <div
+          className={modoEscuro ? "card" : "cardWhite"}
+          data-aos="fade-left"
+          data-aos-delay="200"
+        >
+          <article
+            className={modoEscuro ? "cardResponsivo" : "cardResponsivoWhite"}
+          >
+            <div className={modoEscuro ? "imagensCard" : "imagensCardWhite"}>
+              <img
+                src={
+                  "https://res.cloudinary.com/dwi9yz8wj/image/upload/v1776177302/Captura_de_Tela_341_fomlu7.png"
+                }
+                className="imagem"
+                alt="Notebook"
+              />
+              <button>
+                <a href="https://hdc-host-ai.vercel.app" target="_blank">
+                  Ver Projeto
+                </a>
+              </button>
+            </div>
+          </article>
+          <article className={modoEscuro ? "projeto" : "projetoWhite"}>
+            <h5>HDC Host AI – Plataforma de Hospedagem Inteligente</h5>
+            <p>
+              Plataforma full stack para apresentação e recomendação inteligente
+              de planos de hospedagem,catálogo dinâmico e integração com IA....
+              <a href="https://github.com/Dev-Rafaael/HDC-HOST-AI">Ver Mais</a>
+            </p>
+            <ul className={modoEscuro ? "tecUsadas" : "tecUsadasWhite"}>
+              <li>
+                <img
+                  src={
+                    "https://res.cloudinary.com/dwi9yz8wj/image/upload/v1776177924/icons8-nextjs-96_br6w3i.png"
+                  }
+                  alt="Next Js"
+                />
+              </li>
+              <li>
+                <img
+                  src={
+                    "https://res.cloudinary.com/dwi9yz8wj/image/upload/v1765203589/Tailwind_CSS_Logo.svg_wvczwk.png"
+                  }
+                  alt="Tailwind"
+                />
+              </li>
+              <li>
+                <img
+                  src={
+                    "https://res.cloudinary.com/dwi9yz8wj/image/upload/v1765200126/html_ktpien.png"
+                  }
+                  alt="HTML"
+                />
+              </li>
+              <li>
+                <img
+                  src={
+                    "https://res.cloudinary.com/dwi9yz8wj/image/upload/v1765200187/javascript_nbwfe1.png"
+                  }
+                  alt="JavaScript"
+                />
+              </li>
+
+              <li>
+                <img
+                  src={
+                    "https://res.cloudinary.com/dwi9yz8wj/image/upload/v1765199984/typescript_qvdtda.png"
+                  }
+                  alt="TypesScript"
+                />
+              </li>
+              <li>
+                <img
+                  src={
+                    "https://res.cloudinary.com/dwi9yz8wj/image/upload/v1765200025/node_i7wjfa.png"
+                  }
+                  alt="Node"
+                />
+              </li>
+              <li>
+                <img
+                  src={
+                    "https://res.cloudinary.com/dwi9yz8wj/image/upload/v1765199925/express_fp1g2i.png"
+                  }
+                  alt="Express"
+                />
+              </li>
+              <li>
+                <img
+                  src={
+                    "https://res.cloudinary.com/dwi9yz8wj/image/upload/v1765199892/Postgresql_elephant.svg_nbq4s3.png"
+                  }
+                  alt="PostgreSql"
+                />
+              </li>
+              <li>
+                <img
+                  src={
+                    "https://res.cloudinary.com/dwi9yz8wj/image/upload/v1765200915/prisma-orm_pbkjo6.png"
+                  }
+                  alt="Prisma"
+                />
+              </li>
+              <li>
+                <img
+                  src={
+                    "https://res.cloudinary.com/dwi9yz8wj/image/upload/v1776177921/icons8-jest-96_i0gymv.png"
+                  }
+                  alt="Jest"
+                />
+              </li>
+              <li>
+                <img
+                  src={
+                    "https://res.cloudinary.com/dwi9yz8wj/image/upload/v1776177205/configuracao_ux8fem.png"
+                  }
+                  alt="IA"
+                />
+              </li>
+            </ul>
+          </article>
+        </div>{" "}
         <div
           className={modoEscuro ? "card" : "cardWhite"}
           data-aos="fade-left"
@@ -52,7 +174,8 @@ function Projects({ modoEscuro }) {
             <p>
               Sistema fullstack para agendamento de consultas, permitindo que
               usuários criem e gerenciem agendamentos de forma simples e
-              organizada... <a href="https://github.com/Dev-Rafaael/Agendify">Ver Mais</a>
+              organizada...{" "}
+              <a href="https://github.com/Dev-Rafaael/Agendify">Ver Mais</a>
             </p>
 
             <ul className={modoEscuro ? "tecUsadas" : "tecUsadasWhite"}>
@@ -202,7 +325,10 @@ function Projects({ modoEscuro }) {
             <h5>La Pizza – E-commerce</h5>
             <p>
               E-commerce da pizzaria La Pizza, com cardápio online, pedidos
-              personalizados e dashboard de gestão de vendas... <a href="https://github.com/Dev-Rafaael/LaPizza-Monorepo">Ver Mais</a>
+              personalizados e dashboard de gestão de vendas...{" "}
+              <a href="https://github.com/Dev-Rafaael/LaPizza-Monorepo">
+                Ver Mais
+              </a>
             </p>
             <ul className={modoEscuro ? "tecUsadas" : "tecUsadasWhite"}>
               <li>
@@ -351,7 +477,10 @@ function Projects({ modoEscuro }) {
             <h5>La Pizza – Dashboard Administrativo</h5>
             <p>
               Dashboard para gestão da pizzaria Lá Pizza, com controle de
-              pedidos, clientes e vendas em tempo real... <a href="https://github.com/Dev-Rafaael/LaPizza-Monorepo/tree/main/Apps/Dashboard">Ver Mais</a>
+              pedidos, clientes e vendas em tempo real...{" "}
+              <a href="https://github.com/Dev-Rafaael/LaPizza-Monorepo/tree/main/Apps/Dashboard">
+                Ver Mais
+              </a>
             </p>
             <ul className={modoEscuro ? "tecUsadas" : "tecUsadasWhite"}>
               <li>
@@ -473,7 +602,8 @@ function Projects({ modoEscuro }) {
             <h5>DecoreHome – Catálogo Decorativo</h5>
             <p>
               Projeto apresentando ambientes decorados, com visual moderno e
-              foco em navegação simples para explorar inspirações de design... <a href="https://github.com/Dev-Rafaael/DecoreHome">Ver Mais</a>
+              foco em navegação simples para explorar inspirações de design...{" "}
+              <a href="https://github.com/Dev-Rafaael/DecoreHome">Ver Mais</a>
             </p>
             <ul className={modoEscuro ? "tecUsadas" : "tecUsadasWhite"}>
               <li>
@@ -563,111 +693,6 @@ function Projects({ modoEscuro }) {
             <div className={modoEscuro ? "imagensCard" : "imagensCardWhite"}>
               <img
                 src={
-                  "https://res.cloudinary.com/dwi9yz8wj/image/upload/v1765327561/Captura_de_Tela_174_aeoni5.png"
-                }
-                className="imagem"
-                alt="Notebook"
-              />
-              <button>
-                <a href="https://hdc-host-seven.vercel.app" target="_blank">
-                  Ver Projeto
-                </a>
-              </button>
-            </div>
-          </article>
-          <article className={modoEscuro ? "projeto" : "projetoWhite"}>
-            <h5>HDCHost – Plataforma de Hospedagem</h5>
-            <p>
-              Sistema de hospedagem com painel simples e intuitivo para
-              gerenciar sites, domínios e serviços de forma prática... <a href="https://github.com/Dev-Rafaael/HDC-HOST">Ver Mais</a>
-            </p>
-            <ul className={modoEscuro ? "tecUsadas" : "tecUsadasWhite"}>
-              <li>
-                <img
-                  src={
-                    "https://res.cloudinary.com/dwi9yz8wj/image/upload/v1765200126/html_ktpien.png"
-                  }
-                  alt="HDC Host Image"
-                />
-              </li>
-              <li>
-                <img
-                  src={
-                    "https://res.cloudinary.com/dwi9yz8wj/image/upload/v1765199957/css_wdrfsp.png"
-                  }
-                  alt="CSS"
-                />
-              </li>
-              <li>
-                <img
-                  src={
-                    "https://res.cloudinary.com/dwi9yz8wj/image/upload/v1765200187/javascript_nbwfe1.png"
-                  }
-                  alt="JavaScript"
-                />
-              </li>
-              <li>
-                <img
-                  src={
-                    "https://res.cloudinary.com/dwi9yz8wj/image/upload/v1765200106/react_ejm60z.png"
-                  }
-                  alt="React"
-                />
-              </li>
-              <li>
-                <img
-                  src={
-                    "https://res.cloudinary.com/dwi9yz8wj/image/upload/v1765199984/typescript_qvdtda.png"
-                  }
-                  alt="TypesScript"
-                />
-              </li>
-              <li>
-                <img
-                  src={
-                    "https://res.cloudinary.com/dwi9yz8wj/image/upload/v1765200025/node_i7wjfa.png"
-                  }
-                  alt="Node"
-                />
-              </li>
-              <li>
-                <img
-                  src={
-                    "https://res.cloudinary.com/dwi9yz8wj/image/upload/v1765199925/express_fp1g2i.png"
-                  }
-                  alt="Express"
-                />
-              </li>
-              <li>
-                <img
-                  src={
-                    "https://res.cloudinary.com/dwi9yz8wj/image/upload/v1765199887/zod_uz54r5.png"
-                  }
-                  alt="Zod"
-                />
-              </li>
-              <li>
-                <img
-                  src={
-                    "https://res.cloudinary.com/dwi9yz8wj/image/upload/v1765199887/vite_f5mm9w.png"
-                  }
-                  alt="Vite"
-                />
-              </li>
-            </ul>
-          </article>
-        </div>
-        <div
-          className={modoEscuro ? "card" : "cardWhite"}
-          data-aos="fade-left"
-          data-aos-delay="200"
-        >
-          <article
-            className={modoEscuro ? "cardResponsivo" : "cardResponsivoWhite"}
-          >
-            <div className={modoEscuro ? "imagensCard" : "imagensCardWhite"}>
-              <img
-                src={
                   "https://res.cloudinary.com/dwi9yz8wj/image/upload/v1765455736/Captura_de_Tela_178_negczx.png"
                 }
                 className="imagem"
@@ -684,7 +709,8 @@ function Projects({ modoEscuro }) {
             <h5>Constrularis – Catálogo de Materiais de Construção</h5>
             <p>
               Página moderna para apresentar produtos da construção civil com
-              clareza e fácil navegação... <a href="https://github.com/Dev-Rafaael/Constrularis">Ver Mais</a>
+              clareza e fácil navegação...{" "}
+              <a href="https://github.com/Dev-Rafaael/Constrularis">Ver Mais</a>
             </p>
 
             <ul className={modoEscuro ? "tecUsadas" : "tecUsadasWhite"}>
